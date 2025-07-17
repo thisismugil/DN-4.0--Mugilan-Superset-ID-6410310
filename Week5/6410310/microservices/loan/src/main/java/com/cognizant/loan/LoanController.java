c41c3d19c1298a94d934992d59a9a291f8733e1e
thisismugil <thinknewbro@gmail.com>
2025-07-17 19:27:31 +0530

    Week Completed- Microservices

---
+++ Week5/6410310/microservices/loan/src/main/java/com/cognizant/loan/LoanController.java
@@ -0,0 +1,11 @@
+package com.cognizant.loan;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+    @GetMapping("/{number}")
+    public Loan getLoanDetails(@PathVariable String number) {
+        return new Loan(number, "car", 400000, 3258, 18);
+    }
+    record Loan(String number, String type, double loan, int emi, int tenure) {}
+}
---
+++ Week5/readme.md
@@ -0,0 +1,5 @@
+Loan Service Output - port number: 8081
+![alt text](<loans.jpg>)
+
+Account Service Output - port number: 8080
+![alt text](<Account.jpg>)
